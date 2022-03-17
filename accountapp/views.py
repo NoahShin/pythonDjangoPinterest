13414e3ab7df2b9446db9b6f02cf4cfbd7b3329b
NoahShin <noahshin11@gmail.com>
2022-03-17 16:29:35 +0900

    finish for django python

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,6 @@ from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
-from accountapp.models import HelloWorld
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -15,18 +14,6 @@ from django.contrib.auth.decorators import login_required
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
-
-@login_required
-def hello_world(request):
-    if request.method == 'POST':
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
@@ -46,6 +33,7 @@ class AccountDetailView(DetailView, MultipleObjectMixin):
     def get_context_data(self, **kwargs):
         object_list = Article.objects.filter(writer=self.get_object())
         return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
